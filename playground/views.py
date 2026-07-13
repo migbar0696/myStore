264f3c122c2f79d2b52af42ee07873a7a8a145e8
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-13 11:59:38 +0300

    some models class defined

---
+++ playground/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# def say_hello(request):
+#     return HttpResponse('Hello World')
+
+def say_hello(request):
+
+    return render(request,'hello.html')
