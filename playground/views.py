df3e57d27abb316a2a5423674ee7c3ad72e5594f
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-21 19:00:30 +0300

    Solution for  ' select products that have been ordered and sort them by title'

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.db.models import Q, F
 from django.http import HttpResponse
-from store.models import Product
+from store.models import Product, OrderItem
 
 # def say_hello(request):
 #     return HttpResponse('Hello World')
 
 
 def say_hello(request):
-    # query_set = Product.objects.values('id', 'title', 'collection__title')
-    query_set = Product.objects.values_list('id', 'title', 'collection__title')
-    
+    query_set = Product.objects.filter(id__in =OrderItem.objects.values('product__id').distinct() ).order_by('title')
+
     
     
     
