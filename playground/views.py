160c5ee54426e2bd2525c818607861bf67bf8e20
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-21 17:01:34 +0300

    Complex Lookup using Q objetcs

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.http import HttpResponse
 from store.models import Product
 
@@ -7,7 +8,9 @@ from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.filter(last_update__year = 2021)
+    # query_set = Product.objects.filter(unit_price__lt = 20, inventory__lt = 10)
+    # query_set = Product.objects.filter(unit_price__lt = 20).filter(inventory__lt = 10)
+    query_set = Product.objects.filter(Q(unit_price__lt = 20) | ~Q(inventory__lt=10))
     
-
+    
     return render(request,'hello.html',{'name':'Migbaru', 'products':list(query_set)})
