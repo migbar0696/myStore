060d3bababbed9842e818d7b673a322497830948
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-21 15:19:30 +0300

    Managers and Query sets

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 # def say_hello(request):
 #     return HttpResponse('Hello World')
 
+
 def say_hello(request):
+    query_set = Product.objects.all()
+    
+    for product in query_set:
+        print(product)
 
     return render(request,'hello.html')
--- store/models.py
+++ store/models.py
@@ -12,7 +12,7 @@ class Promotion(models.Model):
     
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=255)
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=5, decimal_places=2)
     inventory = models.IntegerField()
