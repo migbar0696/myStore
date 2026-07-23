f7e565d37e216bbd1df1c810806daf410e38e065
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-23 16:32:25 +0300

    Annotating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Q, F
 from django.db.models.aggregates import Count, Min, Max, Avg, Sum
+from django.db.models import value, F
 from django.http import HttpResponse
-from store.models import Product, OrderItem, Order
+from store.models import Product, OrderItem, Order, Customer
 
 # def say_hello(request):
 #     return HttpResponse('Hello World')
@@ -11,7 +12,8 @@ from store.models import Product, OrderItem, Order
 def say_hello(request):
     # query_set = Product.objects.select_related('collection').prefetch_related('promotions').all()
     # query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
+    # result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
+    result = Customer.objects.annotate(new_id=F('id') + 1)
     
 
     
