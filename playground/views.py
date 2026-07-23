59e6e839d6e67d581c690eaba266283538d421b8
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-23 15:30:09 +0300

    Get the last 5 orders with their customers and items(product)

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.db.models import Q, F
 from django.http import HttpResponse
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 # def say_hello(request):
 #     return HttpResponse('Hello World')
 
 
 def say_hello(request):
-    query_set = Product.objects.select_related('collection').prefetch_related('promotions').all()
+    # query_set = Product.objects.select_related('collection').prefetch_related('promotions').all()
+    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    
 
     
     
     
-    return render(request,'hello.html',{'name':'Migbaru', 'products':list(query_set)})
+    return render(request,'hello.html',{'name':'Migbaru', 'orders':list(query_set)})
