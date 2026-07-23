05f7c6346b346723d13285af9525c5428b8620bc
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-23 16:05:34 +0300

    Aggregating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Q, F
+from django.db.models.aggregates import Count, Min, Max, Avg, Sum
 from django.http import HttpResponse
 from store.models import Product, OrderItem, Order
 
@@ -9,10 +10,11 @@ from store.models import Product, OrderItem, Order
 
 def say_hello(request):
     # query_set = Product.objects.select_related('collection').prefetch_related('promotions').all()
-    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
     
 
     
     
     
-    return render(request,'hello.html',{'name':'Migbaru', 'orders':list(query_set)})
+    return render(request,'hello.html',{'name':'Migbaru', 'result':result})
