537d4906abd9dcf8332b7ba8973b122fba828bc7
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-24 12:37:51 +0300

    Custom Managers

--- playground/views.py
+++ playground/views.py
@@ -9,14 +9,7 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    content_type = ContentType.objects.get_for_model(Product)
-    
-    query_set = TaggedItem.objects\
-        .select_related('tag')\
-        .filter(
-            content_type=content_type,
-            object_id = 1
-    )
+    query_set = TaggedItem.objects.get_tags_for(Product, 1)
      
     
     return render(request,'hello.html',{'name':'Migbaru', 'result':list(query_set)})
--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete = models.SET_NULL, null = True,related_name="+")
