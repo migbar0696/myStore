d9e56ce1845827e48980c04af3bf8a99f98cf165
Migbaru Belay <migbarubelaya@gmail.com>
2026-07-24 13:41:38 +0300

    Updating Objects

--- playground/views.py
+++ playground/views.py
@@ -9,12 +9,18 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = 'Video Games'
-    collection.featured_product = Product(pk=1)
-    collection.save()
+    # collection = Collection(pk=11)
+    # collection.title = 'Games'
+    # collection.featured_product = None
+    # collection.save()
     
-    # collection = Collection.objects.create(title='Video Games', featured_product_id = 1)
+    # collection = Collection.objects.get(pk=11)
+    # collection.featured_product = None
+    # collection.save()
+    
+    
+    
+    collection = Collection.objects.filter(pk=11).update(title='Games', featured_product_id = None)
      
     
     return render(request,'hello.html',{'name':'Migbaru'})
