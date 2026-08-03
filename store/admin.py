a048bf239022691e51487b6f3cb61ee76026334f
Migbaru Belay <migbarubelaya@gmail.com>
2026-08-03 15:28:57 +0300

    Selecting Related Object

--- store/admin.py
+++ store/admin.py
@@ -6,15 +6,20 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+    
+    def collection_title(self, product):
+        return product.collection.title
     
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
             return 'Low'
         return 'Ok'
+    
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -22,6 +27,13 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+    
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','placed_at','customer' ]
+
+
+
     
 admin.site.register(models.Collection)
 
