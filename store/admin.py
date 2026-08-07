266a0cd7d9c670dc6842dbd808a65f59f1546cef
Migbaru Belay <migbarubelaya@gmail.com>
2026-08-07 13:26:13 +0300

    Adding search to the customer's list page

--- store/admin.py
+++ store/admin.py
@@ -30,6 +30,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     
     def order_count(self, customer):
         url = (reverse('admin:store_order_changelist')
